720da6edae9e0f22c22029c46a8e317572b0196b
JNoriegaH <jorgeivan0317@gmail.com>
2022-05-24 11:43:01 -0700

    Excepciones 2

--- manejoDeExcepciones/Program.cs
+++ manejoDeExcepciones/Program.cs
@@ -10,6 +10,22 @@ namespace manejoDeExcepciones
     {
         static void Main(string[] args)
         {
+            int numero;
+            try
+            {
+                Console.Write("Ingrese un numero entero: ");
+                numero = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("El cuadrado es {0}", Math.Pow(numero, 2));
+            }
+            catch (FormatException s)
+            {
+                Console.WriteLine(s.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
